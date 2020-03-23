5271048c24b81c4649f5c736c7ca23d2307e3463
Rad <chihaby@gmail.com>
2020-03-23 14:06:26 -0700

    hides bad words

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.sanitizeContent = functions.firestore
+  .document('posts/{postId}/comments/{commentId}')
+  .onWrite(async change => {
+    if (!change.after.exists) return;
+
+    const { content, sanitized } = change.after.data();
+
+    if (content && !sanitized) {
+      return change.after.ref.update({
+        content: content.replace(/fuck/g, '****'),
+        sanitize: true
+      });
+    }
+    return null;
+  });
